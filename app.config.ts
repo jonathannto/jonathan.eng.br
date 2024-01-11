89baacfd0549a446ae9965bcb6f50204a5d3d2ea
Jonathan Nascimento <nascimento.jonathan@live.com>
2024-01-11 15:09:29 -0300

    Ajustes site jou

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Jonathan Nascimento',
+    description: 'Meu site',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -11,9 +11,9 @@ export default defineAppConfig({
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/logo-jou.png', // path of the logo
+        pathDark: '/logo-jou-dark.png', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'Jonathan Nascimento' // alt of the logo
       }
     },
     footer: {
@@ -23,16 +23,27 @@ export default defineAppConfig({
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: 'Minhas redes sociais' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      x: {
+        icon: 'pajamas:x',
+        label: 'X (Ex-twitter)',
+        href:'https://twitter.com/jonathanNto'
+      },
+      threads: {
+        icon: 'bi:threads',
+        label: 'Threads',
+        href:'https://www.threads.net/@jonathan.nto'
+      },
+      instagram: 'jonathan.nto',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+        href: 'https://www.linkedin.com/in/jonathan-nto/'
+      },
+      youtube: '@jonathannto',
+      github: 'jonathannto',
     },
     form: {
       successMessage: 'Message sent. Thank you!'
